f5f22a32654aacfc8b0021ae7665f5658fef29ca
bintangwintang <zaid.s.pawaskar@student.uts.edu.au>
2025-05-07 13:18:49 +1000

    Add saveUser() helper and persist goals to UserManager

--- a3-fitness-app/a3-fitness-app/ViewModels/UserManager.swift
+++ a3-fitness-app/a3-fitness-app/ViewModels/UserManager.swift
@@ -6,46 +6,60 @@ class UserManager: ObservableObject {
     
     private let url = URL.documentsDirectory.appending(path: "user.json")
     
-    func createUser(name: String, level: Level) { // CREATE
+    // CREATE
+    func createUser(name: String, level: Level) {
         self.user = User(name: name, level: level)
-        
         do {
             try JSONEncoder().encode(self.user).write(to: url)
-            
         } catch {
             print(error.localizedDescription)
         }
     }
     
-    func readUser() { // READ
+    // READ
+    func readUser() {
         do {
-            self.user = try JSONDecoder().decode(User.self, from: Data(contentsOf: url))
+            self.user = try JSONDecoder()
+                .decode(User.self, from: Data(contentsOf: url))
         } catch {
             print(error.localizedDescription)
         }
     }
     
-    func updateUser(name: String, level: Level, workouts: [Workout]?) { // UPDATE
+    // UPDATE
+    func updateUser(name: String, level: Level, workouts: [Workout]?) {
         self.user = User(name: name, level: level, workouts: workouts)
-            
         do {
-            try JSONEncoder().encode(self.user).write(to: url, options: [.atomic])
-            
+            try JSONEncoder()
+                .encode(self.user)
+                .write(to: url, options: [.atomic])
         } catch {
             print(error.localizedDescription)
         }
     }
     
-    func deleteUser() { // DELETE
+    // DELETE
+    func deleteUser() {
         do {
             let fileManager = FileManager.default
-            
             if fileManager.fileExists(atPath: url.path) {
                 try fileManager.removeItem(at: url)
                 self.user = User(name: "", level: Level(level: 1, xp: 0))
             }
         } catch {
             print(error.localizedDescription)
+        }
+    }
+    
+    // SAVE
+    /// Persist the current `user` back to disk.
+    func saveUser() {
+        do {
+            try JSONEncoder()
+                .encode(self.user)
+                .write(to: url, options: [.atomic])
+        } catch {
+            print("Error saving user:", error.localizedDescription)
         }
     }
 }
